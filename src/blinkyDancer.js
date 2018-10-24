ef2692bbfbd62a8595fe1119f355d07d369a83b1
Jessica Herrera <jherrera@fb.com>
2018-10-23 23:13:27 -0700

    Complete pseudoclassical refacto

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,27 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+  //call dancer
+  makeDancer.call(this, top, left, timeBetweenSteps);
 
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
+};
 
-  var oldStep = blinkyDancer.step;
 
-  blinkyDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
+//set up prototypes
+makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
 
-  return blinkyDancer;
+//set up constructor
+makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
+
+//makeBlinkyDancer.prototype.step >> points to makeDancer.step
+//save old step function before over writing it
+makeBlinkyDancer.prototype.oldStep = makeBlinkyDancer.prototype.step;
+
+//over writes makeDancer.step
+makeBlinkyDancer.prototype.step = function() {
+  // call the old version of step at the beginning of any call to this new version of step
+  this.oldStep();
+
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.toggle();
 };
